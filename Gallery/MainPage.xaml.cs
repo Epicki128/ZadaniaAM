60d690b29395bc69fb52b59b1a71b6bd511626f2
Epicki128 <epicki128@proton.me>
2025-10-17 00:07:13 +0200

    Dodaj pliki projektów.

---
+++ Gallery/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿namespace Gallery
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        String[] sources = { "kot1.jpg", "kot2.jpg", "kot3.jpg", "kot4.jpg" };
+        int counter = 0;
+
+        void Carousel(object Sender, EventArgs e)
+        {
+            if (Sender.Equals(prevBtn))
+            {
+                if (counter > 0)
+                {
+                    counter--;
+                }
+                else
+                {
+                    counter = 3;
+                }
+
+            }
+            else
+            {
+                if (counter < 3)
+                {
+                    counter++;
+                }
+                else
+                {
+                    counter = 0;
+                }
+
+            }
+            imageCarousel.Source = sources[counter];
+        }
+
+        void Entry(object Sender, TextChangedEventArgs e)
+        {
+            int temp = 0;
+            try { temp = int.Parse(imgIdEnt.Text) - 1; } catch { }
+
+            if (temp >= 0 && temp <= 3)
+            {
+                counter = temp;
+            }
+
+            imageCarousel.Source = sources[counter];
+        }
+
+        private void bgChange(object sender, ToggledEventArgs e)
+        {
+
+            if (e.Value)
+            {
+                main.SetDynamicResource(VisualElement.StyleProperty, "bgBlue");
+            }
+            else
+            {
+                main.SetDynamicResource(VisualElement.StyleProperty, "bgGreen");
+            }
+        }
+    }
+}
