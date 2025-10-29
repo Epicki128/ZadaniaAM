98f25d7a100b789483263aec8e12661a87408ab4
Epicki128 <epicki128@proton.me>
2025-10-30 00:28:53 +0100

    Finished assigment "Greetings"

---
+++ Greetings/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace Greetings
+{
+    public partial class MainPage : ContentPage
+    {
+        private List<string> Greetings = new List<string> { "Dzień dobry", "Buenos dias", "Good morning" };
+        private int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Quote.Text = Greetings[count];
+        }
+
+        private void FontSizeChanged(object sender, ValueChangedEventArgs e)
+        {
+            int value = (int)FontSizeSlider.Value;
+            Output.Text = $"Rozmiar {value}";
+            Quote.FontSize = value;
+        }
+
+        private void BtnClicked(object sender, EventArgs e)
+        {
+            if (count < Greetings.Count - 1)
+            {
+                count++;
+            }
+            else
+            {
+                count = 0;
+            }
+
+            Quote.Text = Greetings[count];
+        }
+    }
+}
---
+++ Greetings/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Greetings
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
