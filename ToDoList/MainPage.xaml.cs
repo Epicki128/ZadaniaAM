29500bb79f34c3377d30803eb29a33d3d19e7972
Epicki128 <epicki128@proton.me>
2025-11-22 18:01:40 +0100

    Finished Assigment "Simple Mobile ToDo List"

---
+++ ToDoList/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace ToDoList
+{
+    public partial class MainPage : ContentPage
+    {
+        private List<string> todos = new List<string>() { "Zakupy: Chleb, masło, ser", "Do zrobienie: obiad, umyć podłogi", "weekend: kino, spacer z psem" };
+        public MainPage()
+        {
+            InitializeComponent();
+            LvTodos.ItemsSource = todos;
+        }
+
+        private void addTodo(object sender, EventArgs e)
+        {
+            string temp = EntTodo.Text;
+            todos.Add(temp);
+            LvTodos.ItemsSource = todos.ToArray();
+        }
+    }
+}
